555eb66eeec8bb5b6cd2f30bcdcde62769a719f7
mrchcat <m-cat@mail.ru>
2025-07-05 16:50:48 +0300

    feat: front_transfer

---
+++ front/src/main/java/com/github/mrchcat/front/dto/NonCashTransferRequest.java
@@ -0,0 +1,19 @@
+package com.github.mrchcat.front.dto;
+
+import com.github.mrchcat.front.model.BankCurrency;
+import com.github.mrchcat.front.model.TransferDirection;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+
+@Builder
+public record NonCashTransferRequest(TransferDirection direction,
+                                     BankCurrency fromCurrency,
+                                     BankCurrency toCurrency,
+                                     BigDecimal amount,
+                                     String fromUsername,
+                                     String toUsername) {
+}
