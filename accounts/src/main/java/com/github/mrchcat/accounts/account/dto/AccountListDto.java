37e11b54bfa57bb180edc09b400c5435cf5982b7
mrchcat <m-cat@mail.ru>
2025-07-12 17:27:11 +0300

    refactor: все общие классы пересены в одну папку

--- accounts/src/main/java/com/github/mrchcat/accounts/account/dto/AccountListDto.java
+++
@@ -1,9 +0,0 @@
-package com.github.mrchcat.accounts.account.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-import java.util.List;
-import java.util.UUID;
-
-public record AccountListDto(@NotNull List<UUID> accountsId) {
-}
--- accounts/src/main/java/com/github/mrchcat/accounts/account/dto/BankCurrencyListDto.java
+++
@@ -1,8 +0,0 @@
-package com.github.mrchcat.accounts.account.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-import java.util.List;
-
-public record BankCurrencyListDto(@NotNull List<String> string_code_iso4217) {
-}
